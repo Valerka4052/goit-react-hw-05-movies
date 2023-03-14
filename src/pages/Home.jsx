ad3c5d190f8aa99ce9306bf57cb1334a091a6be7
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-03-14 20:51:22 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { getTrendingMovies } from "api"
 import { MovieList } from "components/MovieList/MovieList";
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 
 export function Home() {
     const [movies, setMovies] = useState([]);
